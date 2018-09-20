1bc69c8c20d781346c2b5f21045fef2d5fa5ae75
AkioMabuchi <38254498+AkioMabuchi@users.noreply.github.com>
2018-09-20 22:17:32 +0900

    Add files via upload

---
+++ BallManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BallManager : MonoBehaviour {
+
+    private GameObject gameManager;
+
+	// Use this for initialization
+	void Start () {
+        gameManager = GameObject.Find("GameManager");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        bool isBallMoving = gameManager.GetComponent<GameManager>().isBallMoving;
+        if(isBallMoving){
+            float velocityY = GetComponent<Rigidbody2D>().velocity.y;
+            if(velocityY < 0.8f && velocityY > 0.0f){
+                Debug.Log("低速上昇補正システム、作動中");
+                GetComponent<Rigidbody2D>().AddForce(new Vector2(0.0f, 0.5f), ForceMode2D.Force);
+            }else if(velocityY > -0.8f && velocityY <= 0.0f){
+                Debug.Log("低速落下補正システム、作動中");
+                GetComponent<Rigidbody2D>().AddForce(new Vector2(0.0f, -0.5f), ForceMode2D.Force);
+            }
+        }
+	}
+
+    void OnCollisionEnter2D(Collision2D collision){
+        Debug.Log("Collision");
+        switch(collision.gameObject.tag){
+            case "Block":
+                gameManager.GetComponent<GameManager>().DestroyBlock(collision.gameObject);
+                gameManager.GetComponent<GameManager>().AddScore(10);
+                break;
+
+            case "Racket":
+                Debug.Log("Racket");
+                float velocityZero = 5.0f;
+                float ballPointX = collision.contacts[0].point.x;
+                float racketPointX = collision.gameObject.transform.position.x;
+                float pointX = ballPointX - racketPointX;
+                float degree = (1.0f - pointX) * Mathf.PI / 2;
+                Vector2 force = new Vector2(velocityZero * Mathf.Cos(degree), velocityZero * Mathf.Sin(degree));
+
+
+                GetComponent<Rigidbody2D>().velocity = new Vector2(0.0f, 0.0f);
+                GetComponent<Rigidbody2D>().AddForce(force, ForceMode2D.Impulse);
+                break;
+            case "OutArea":
+                gameManager.GetComponent<GameManager>().FallBall();
+                break;
+
+        }
+    }
+}
+
+// ラケットが左端に衝突した場合は135°の方向に、右端の場合は45°の方向に、真ん中の場合は90°の方向に、
+// θ = (1 - x)π/2
+// Vx = Vcosθ
+// Vy = Vsinθ
---
+++ GameManager.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    public const float LEFTEST_POSITION = -3.4f;
+    public const float RIGHTEST_POSITION = 1.4f;
+
+	public GameObject ball;
+
+    public Text textStage;
+    public Text textScore;
+    public Text textNumberOfBall;
+    public Text textNumberOfBlock;
+    public Text textStart;
+
+    public GameObject[] blocksPrefab = new GameObject[7];
+
+	public GameObject racket;
+
+    public GameObject imageGameOver;
+    public GameObject imageStageClear;
+
+    public int numberOfBlock = 0;
+    public int numberOfBall = 2;
+
+    public int theStage = 1;
+    public int theScore = 0;
+
+    public bool isBallMoving = false;
+
+	// Use this for initialization
+    void Start () {
+        for (int i = 0; i < 10; i++){
+            for (int j = 0; j < 7;j++){
+                float positionX = i * 0.5f - 3.25f;
+                float positionY = j * -0.2f + 2.5f;
+                CreateBlock(blocksPrefab[j], positionX, positionY);
+            }
+        }
+        ChangeTextNumberOfBall();
+        ChangeTextNumberOfBlock();
+    }
+
+	// Update is called once per frame
+	void Update () {
+        if (isBallMoving){
+            float temporaryMousePositionX = (Input.mousePosition.x - 400.0f) / 100.0f;
+
+            if (temporaryMousePositionX < LEFTEST_POSITION){
+                temporaryMousePositionX = LEFTEST_POSITION;
+            }
+            else if (temporaryMousePositionX > RIGHTEST_POSITION){
+                temporaryMousePositionX = RIGHTEST_POSITION;
+            }
+
+            racket.transform.position = new Vector3(temporaryMousePositionX, -2.5f, 0.0f);
+        }
+	}
+
+    public void PushGoToTitleButton(){
+        SceneManager.LoadScene("TitleScene");
+    }
+
+    public void PushStartBallButton(){
+        if (!isBallMoving){
+            isBallMoving = true;
+            ball.GetComponent<Rigidbody2D>().AddForce(new Vector2(0.0f, -1.0f), ForceMode2D.Impulse);
+            textStart.enabled = false;
+        }
+    }
+
+    public void CreateBlock(GameObject block, float positionX, float positionY){
+        Instantiate(block, new Vector3(positionX, positionY, 0.0f), new Quaternion(0.0f, 0.0f, 0.0f, 0.0f));
+        numberOfBlock++;
+    }
+
+    public void DestroyBlock(GameObject block){
+        Destroy(block);
+        numberOfBlock--;
+        if(numberOfBlock <= 0){
+            StopBall();
+            Debug.Log("CLEAR");
+            AddScore(300);
+            imageStageClear.SetActive(true);
+        }
+        ChangeTextNumberOfBlock();
+    }
+
+    public void StopBall(){
+        isBallMoving = false;
+        ball.GetComponent<Rigidbody2D>().velocity = new Vector2(0.0f, 0.0f);
+    }
+
+    public void FallBall(){
+        StopBall();
+
+        if(numberOfBall > 0){
+            numberOfBall--;
+            ChangeTextNumberOfBall();
+            ball.transform.position = new Vector3(-1.0f, -2.0f, 0.0f);
+            racket.transform.position = new Vector3(-1.0f, -2.5f, 0.0f);
+            textStart.enabled = true;
+        }else{
+            Destroy(ball);
+            Debug.Log("GAME OVER");
+            imageGameOver.SetActive(true);
+        }
+    }
+
+
+
+    public void ChangeTextNumberOfBall(){
+        textNumberOfBall.text = "BALL   " + numberOfBall;
+    }
+
+    public void ChangeTextNumberOfBlock(){
+        textNumberOfBlock.text = "BLOCK   " + numberOfBlock;
+    }
+
+    public void AddScore(int score){
+        theScore += score;
+        textScore.text = "SCORE   " + theScore;
+    }
+}
